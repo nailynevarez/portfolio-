8afd66e7fe050853ba4855574384dc62d8055f80
nailynevarez <nailynevarez@gmail.com>
2018-08-08 15:45:53 -0700

    Add Raza Unida Conference content.

--- imports/components/Work1.js
+++ imports/components/Work1.js
@@ -22,30 +22,37 @@ export default class Work1 extends Component {
     return (
       <ErrorBoundary>
       <div className= "Work1Text">
-      <h1>UO Muxeres</h1>
-      <h3>LOGO DESIGN</h3>
+      <h1>Raza Unida Conference</h1>
+      <h3>T-SHIRT DESIGN</h3>
       <p className = {this.state.showWork1 ? 'fadeIn' : 'fadeOut'}>
-      Nominavi incorrupte te quo, id mei adipisci antiopam. Saepe iudico
-      commodo ut usu, et impetus expetenda referrentur sed, te soluta prompta
-      impedit sit. Justo eripuit interesset eos ad, pri cu assueverit concludaturque.
-      Erroribus laboramus nec no, no qui nihil graece percipitur.
+      The Raza Unida Youth Conference is an annual conference held at the University of
+      Oregon by the student organization MEChA. This conference invites high school students
+      of latino heritage from all across Oregon to the UO campus to encourage, inspire and empower them to pursue
+      a higher education.
+      <br/>
+      <br/>
+      The coordinators of the 14th annual conference had established the theme “Floreciendo Juntos” (Flourishing Together),
+       and were in search of a shirt design that would embody growth and resilience. As the Graphic Designer for MEChA,
+       I worked with the conference coordinators to create a design that ultimately excited the youth and united our community.
+       The design was printed on over 300 shirts and was worn by attendees, volunteers, and workshop leaders at the conference.
       </p>
       <p className = {this.state.showWork1 ? 'fadeIn' : 'fadeOut'}>
-        Some more text, maybe a link to something or what my role was in a team.
+        <b>My role:</b> <br/>Graphic Designer <br/><br/>
+        <b>Conference Coordinators:</b> <br/><a>Yomaira Tarula </a> Veronica Alvaro & Ana Osorio
       </p>
       </div>
       <div className= "Work1Img">
-      <img src = '/images/muxeres.png' className = {this.state.showAbout ? 'fadeIn' : 'fadeOut'}/>
+      <img src = '/images/razaunida1.jpg' className = {this.state.showAbout ? 'fadeIn' : 'fadeOut'}/>
       </div>
 
       {this.state.showWork1 ?
       <div className= "Work1links">
-      <Link to = '/work/work1'>Work 1</Link>
-      <Link to = '/work/work2'>Work 2</Link>
-      <Link to = '/work/work3'>Work 3</Link>
-      <Link to = '/work/work4'>Work 4</Link>
-      <Link to = '/work/work5'>Work 5</Link>
-      <Link to = '/work/work6'>Work 5</Link>
+      <Link to = '/work/work1'>Raza Unida Conference</Link>
+      <Link to = '/work/work2'>Majesty</Link>
+      <Link to = '/work/work3'>Women for Women</Link>
+      <Link to = '/work/work4'>OR Kids</Link>
+      <Link to = '/work/work5'>Dear Future Community</Link>
+      <Link to = '/work/work6'>Yanin Art</Link>
       </div>
       : null}
       </ErrorBoundary>
--- imports/components/Work2.js
+++ imports/components/Work2.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import ErrorBoundary from './ErrorBoundary.js'
+import ErrorBoundary from './ErrorBoundary.js';
+import { BrowserRouter, Route, Link, Switch } from "react-router-dom";
 import '/stylesheets/css/app.css';
 
 
@@ -16,14 +17,31 @@ export default class Work2 extends Component {
   render() {
     return (
       <ErrorBoundary>
-
-
-      {this.state.showWork2 ?
-      <div className= "WorkParagraphs">
-      <p id = "firstP" className = {this.state.showWork2 ? 'fadeIn' : 'fadeOut'}>This is work 2</p>
-
+      <div className= "Work2Text">
+      <h1>UO Muxeres</h1>
+      <h3>LOGO DESIGN</h3>
+      <p className = {this.state.showWork2 ? 'fadeIn' : 'fadeOut'}>
+      Nominavi incorrupte te quo, id mei adipisci antiopam. Saepe iudico
+      commodo ut usu, et impetus expetenda referrentur sed, te soluta prompta
+      impedit sit. Justo eripuit interesset eos ad, pri cu assueverit concludaturque.
+      Erroribus laboramus nec no, no qui nihil graece percipitur.
+      </p>
+      <p className = {this.state.showWork2 ? 'fadeIn' : 'fadeOut'}>
+        Some more text, maybe a link to something or what my role was in a team.
+      </p>
       </div>
-      : null}
+      <div className= "Work2Img">
+      <img src = '/images/muxeres.png' className = {this.state.showWork2 ? 'fadeIn' : 'fadeOut'}/>
+      </div>
+
+      <div className= "Work2links">
+      <Link to = '/work/work1'>Raza Unida Conference</Link>
+      <Link to = '/work/work2'>Majesty</Link>
+      <Link to = '/work/work3'>Women for Women</Link>
+      <Link to = '/work/work4'>OR Kids</Link>
+      <Link to = '/work/work5'>Dear Future Community</Link>
+      <Link to = '/work/work6'>Yanin Art</Link>
+      </div>
 
       </ErrorBoundary>
     );
